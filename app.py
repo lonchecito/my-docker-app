ce838032e5211c58513df9a91f72ab2539aeaf18
matias gonzalez <c.matias.ferreyra@gmail.com>
2025-10-15 19:37:06 -0600

    Commit inicial de la aplicacion

---
+++ app.py
@@ -0,0 +1,7 @@
+# app.py
+def main():
+    print("¡Hola! Este mensaje se está ejecutando desde el archivo ejecutable.")
+    print("El proyecto está funcionando dentro de un contenedor Docker.")
+
+if __name__ == "__main__":
+    main()
